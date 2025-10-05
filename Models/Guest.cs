5af93f304bbd7260665da4e31b589d0c4ad7aaf2
Majo <maria.sandoval.o@ucb.edu.bo>
2025-10-05 19:25:02 -0400

    Fix: Guest Model Savings

--- Models/Guest.cs
+++ Models/Guest.cs
@@ -1,6 +1,10 @@
-﻿namespace apiwithdb.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace apiwithdb.Models
 {
     public class Guest
     {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public bool Confirmed { get; set; }
     }
 }
--- Models/dtos/CreateGuestDto.cs
+++ Models/dtos/CreateGuestDto.cs
@@ -1,6 +1,11 @@
-﻿namespace apiwithdb.Models.dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiwithdb.Models.dtos
 {
-    public class CreateGuestDto
+    public record CreateGuestDto
     {
+        [Required, StringLength(100)]
+        public string FullName { get; set; } = string.Empty;
+        public bool Confirmed { get; set; }
     }
 }
