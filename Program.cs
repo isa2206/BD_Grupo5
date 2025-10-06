d9594fd08ab29a63fe40324409f230e3310691cf
Majo <maria.sandoval.o@ucb.edu.bo>
2025-10-05 22:24:20 -0400

    Fix: add of services of guests on program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +13,9 @@ builder.Services.AddOpenApi();
 builder.Services.AddScoped<apiwithdb.Services.IBookService, apiwithdb.Services.BookService>();
 builder.Services.AddScoped<apiwithdb.Repositories.IBookRepository, apiwithdb.Repositories.BookRepository>();
 
+builder.Services.AddDbContext<apiwithdb.Data.AppDbContextGuests>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddScoped<apiwithdb.Services.IGuestService, apiwithdb.Services.GuestService>();
+builder.Services.AddScoped<apiwithdb.Repositories.IGuestRepository, apiwithdb.Repositories.GuestRepository>();
 var app = builder.Build();
 // Dependency Injection
 
