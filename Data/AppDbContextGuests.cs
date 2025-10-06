14fcef0a59a4a8946daa03510ea31af3d902df49
Majo <maria.sandoval.o@ucb.edu.bo>
2025-10-05 20:25:21 -0400

    AppDbContext creation for guests

---
+++ Data/AppDbContextGuests.cs
@@ -0,0 +1,22 @@
+﻿using apiwithdb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiwithdb.Data
+{
+    public class AppDbContextGuests : DbContext
+    {
+        public AppDbContextGuests(DbContextOptions<AppDbContextGuests> options) : base(options) { }
+
+        public DbSet<Guest> Guests => Set<Guest>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Guest>(g =>
+            {
+                g.HasKey(x => x.Id);
+                g.Property(x => x.FullName).IsRequired().HasMaxLength(100);
+                g.Property(x => x.Confirmed);
+            });
+        }
+    }
+}
